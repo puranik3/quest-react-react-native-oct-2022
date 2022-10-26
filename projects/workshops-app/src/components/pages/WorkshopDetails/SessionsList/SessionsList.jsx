34e6ca284e2bb4099dfb68673793b99663d9bb88
Prashanth Puranik <puranik@digdeeper.in>
2022-10-26 08:57:30 +0530

    list of sessions (names)

--- projects/workshops-app/src/components/pages/WorkshopDetails/SessionsList/SessionsList.jsx
+++ projects/workshops-app/src/components/pages/WorkshopDetails/SessionsList/SessionsList.jsx
@@ -1,12 +1,44 @@
-// EXERCISE: Fetch the sessions for the workshop and display the session names
+import { useState, useEffect } from 'react';
+
+import { getSessionsForWorkshopWithId } from '../../../../services/sessions';
+
 const SessionsList = ( { id } ) => {
-    /* your code */
-    
+    const [ loading, setLoading ] = useState( true );
+    const [ sessions, setSessions ] = useState( [] );
+    const [ error, setError ] = useState( null );
+
+    useEffect(
+        () => {
+            const helper = async () => {
+                try {
+                    const data = await getSessionsForWorkshopWithId( id );
+                    setSessions( data );
+                } catch( error ) {
+                    setError( error );
+                } finally {
+                    setLoading( false );
+                }
+            }
+
+            // when page loads, the id will be null and we do not want to make a call to the backend as we do not have workshop id yet
+            if( id ) {
+                helper();
+            }
+        },
+        [ id ] // run the effect whenever a new value for id is sent as prop from the details component
+    );
+
     return (
         <div>
             <h2>List of sessions</h2>
             <hr />
-            {/* your code */}
+            {
+                sessions.map(
+                    session => (
+                        <div key={session.id}>{session.name}</div>
+                    )
+                )
+            }
         </div>
     );
 };
