855aaea0183bc5070c26d24c24b78fca0c486069
Prashanth Puranik <puranik@digdeeper.in>
2022-11-05 12:39:54 +0530

    search suggestions shown

--- projects/workshops-app/src/components/pages/Home/Home.jsx
+++ projects/workshops-app/src/components/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectTheme } from '../../../reducers/theme';
+import { selectItems } from '../../../reducers/search';
 import {
     fetchItemsThunk
 } from '../../../actions/creators';
@@ -8,12 +9,13 @@ import {
 const Home = () => {
     const dispatch = useDispatch();
     const theme = useSelector( selectTheme );
+    const items = useSelector( selectItems );
     const [ searchKey, setSearchKey ] = useState( '' );
 
     const fetchSearchItems = ( event ) => {
         setSearchKey( event.target.value );
 
-        // the action returned by this is a function
+        // the action returned by this is a function - so thunk middleware will execute this function
         dispatch( fetchItemsThunk( event.target.value ) );
     };
 
@@ -22,13 +24,24 @@ const Home = () => {
             <h1>Workshops App</h1>
             <hr />
 
-            <input
-                type="search"
-                value={searchKey}
-                onChange={fetchSearchItems}
-            />
-            
-            You typed: {searchKey}
+            <div className="alert alert-secondary">
+                <input
+                    type="search"
+                    value={searchKey}
+                    onChange={fetchSearchItems}
+                />
+                
+                You typed: {searchKey}
+
+                <h3>Suggestions</h3>
+                {
+                    items.map(
+                        item => (
+                            <div key={item.id}>{item.name}</div>
+                        )
+                    )
+                }
+            </div>
 
             <p>
                 The Workshops application serves details of (fictitious) technical workshops happening in various cities. Every workshop has a broad topic (eg. JavaScript), and a workshop has many sessions (each session covers a sub-topic, eg. Closures in JavaScript).
