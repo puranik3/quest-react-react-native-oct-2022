04e9b2b211ea2ded3e55a8fd85f39d6336d8f1c6
Prashanth Puranik <puranik@digdeeper.in>
2022-11-05 09:20:43 +0530

    working on loading tests

--- projects/workshops-app/src/components/pages/WorkshopsList/WorkshopsList.test.jsx
+++ projects/workshops-app/src/components/pages/WorkshopsList/WorkshopsList.test.jsx
@@ -2,10 +2,20 @@ import WorkshopsList from "./WorkshopsList";
 import { render, screen } from '@testing-library/react';
 
 describe( 'WorkshopsList', () => {
-    test( 'should show the loading message initially', () => {
-        render( <WorkshopsList /> );
+    describe( 'on load', () => {
+        test( 'should show the loading message', () => {
+            render( <WorkshopsList /> );
+    
+            const loadingMessage = screen.getByText( 'Loading...' );
+            expect( loadingMessage ).toBeInTheDocument();
+        });
 
-        const loadingMessage = screen.getByText( 'Loading...' );
-        expect( loadingMessage ).toBeInTheDocument();
+        test( 'should fetch and show the workshops', () => {
+            render( <WorkshopsList /> );
+
+            for( let i = 0; i < 10; i++ ) {
+                
+            }
+        });
     });
 });
