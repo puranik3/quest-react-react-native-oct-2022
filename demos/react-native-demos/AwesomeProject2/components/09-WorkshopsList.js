cfb38e58f159149d74bea4d70a98ff2fd52552f1
Prashanth Puranik <puranik@digdeeper.in>
2022-11-11 07:29:22 +0530

    fetching data

--- demos/react-native-demos/AwesomeProject2/components/09-WorkshopsList.js
+++ demos/react-native-demos/AwesomeProject2/components/09-WorkshopsList.js
@@ -1,7 +1,33 @@
+import { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import MySafeAreaView from './MySafeAreaView';
 
+import axios from 'axios';
+
+// We can use either fetch() API or a third-party library like axios to fetch data
 const WorkshopsList = () => {
+    const [ loading, setLoading ] = useState( true );
+    const [ workshops, setWorkshops ] = useState( [] );
+    const [ error, setError ] = useState( null );
+
+    useEffect(
+        () => {
+            const helper = async () => {
+                try {
+                    const data = await getWorkshops( page );
+                    setWorkshops( data );
+                } catch( error ) {
+                    setError( error );
+                } finally {
+                    setLoading( false );
+                }
+            }
+
+            helper();
+        },
+        []
+    );
+
     return (
         <MySafeAreaView>
             <View style={[ styles.container ]}>
